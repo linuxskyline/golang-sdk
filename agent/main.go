74576d52f9043feeccc7102fb1804c795b3c4258
Marcus Hann <marcus@hhra.me>
2019-10-20 20:02:54 +0100

    Add new function

--- agent/main.go
+++ agent/main.go
@@ -14,6 +14,15 @@ type Client struct {
 	httpClient *http.Client
 }
 
+func newClient(url *url.URL, token string) *Client {
+	return &Client{
+		BaseURL:    url,
+		UserAgent:  "golang/sdkclient",
+		Token:      token,
+		httpClient: &http.Client{},
+	}
+}
+
 func (c *Client) ListHosts() ([]sky.Host, error) {
 	return []sky.Host{}, nil
 }
